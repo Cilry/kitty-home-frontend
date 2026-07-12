2418a23735274443f278a1947254684e163c8125
Cilry <wusanle23@gmail.com>
2026-07-12 17:40:39 +0800

    fix: import welcom

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode, useState } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Welcome from './Welcome.jsx'
+import Welcome from './welcome.jsx'
 
 function Root() {
   const [entered, setEntered] = useState(false)
